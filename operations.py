e7cb3163c11cc0e2d98a3b73ac28031387b9391e
RoboticsAAA20Group <hans.preinfalk.davila@gmail.com>
2025-03-31 12:53:16 -0600

    addition & subtraction in operations file

--- operations.py
+++ operations.py
@@ -1,8 +1,8 @@
-a = int(input(“Give me the first number:”))
-b = int(input(“Give me the second number:”))
+a = int(input("Give me the first number:"))
+b = int(input("Give me the second number:"))
 
 # Display the addition
+print(a + b)
 
 # Display the subtraction
-
-# Display multiplication
+print(a - b)
